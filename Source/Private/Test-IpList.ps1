347a746aceb5eb7dc252d01decc96a78eeb8bf95
sar <sebastian.alighierir@uanl.edu.mx>
2025-08-30 21:01:36 -0600

    Add logig to test Test-IpList or use Get-NetworkProcess

--- Source/Private/Test-IpList.ps1
+++ Source/Private/Test-IpList.ps1
@@ -1,8 +1,13 @@
 function TestIpList {
-    param (
-    [array]$IpList
+    param(
+    [bool]$IsTest=$false
     )
 
+    if ($IsTest){
+        $IpList=@(136.34.156.82,14.103.45.20,104.26.12.38,167.94.138.137)
+    } else {
+        $IpList=Get-NetworkProcess -ReturnIpList True
+    }
     $ResponseList=@()
 
     if (-not $IpList) {
