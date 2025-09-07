0470f35f818f7c6605d2126c017b715bf3ff80ce
sar <sebastian.alighierir@uanl.edu.mx>
2025-09-07 16:18:30 -0600

    Add first version of the menu

---
+++ Source/Public/Select-ForensicTool.ps1
@@ -0,0 +1,27 @@
+function Show-Menu {
+
+    $Prompt="Select an option:
+    1) Get-EventReport
+    2) Get-NetworkProcess
+    3) Test-IpList"
+
+    while ($true) {
+        Write-Host ""
+        Write-Host $Prompt -NoNewline
+        $selection = Read-Host " "
+        switch ($selection.Trim()) {
+            '1' {
+                Get-EventReport
+            }
+            '2' {
+                Get-NetworkProcess
+            }
+            '3' {
+                Test-IpList -Test
+            }
+            default {
+                Write-Warning "Invalid selection. Please enter 1, 2, or 3."
+            }
+        }
+    }
+}
